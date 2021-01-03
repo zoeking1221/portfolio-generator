fb832a3a07d9da446f6950178c773d3c05dc4bc6
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2021-01-02 22:33:18 -0500

    created function to create project section

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -146,5 +140,19 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+    });
   });
+
+// const mockData = {
+//   name: 'Lernantino',
+//   github: 'lernantino',
+//   projects: []
+// }
+
+// const pageHTML = generatePage(mockData);
